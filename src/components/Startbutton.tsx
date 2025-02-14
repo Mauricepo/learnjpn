b0cb52bd215dffaab3b39cff6cfd001180023965
Maurice Popenko <maurice.popenko@malteser.org>
2025-02-14 16:21:27 +0100

    ...

--- src/components/Startbutton.tsx
+++ src/components/Startbutton.tsx
@@ -1,13 +1,10 @@
 import { Center } from '@mantine/core'
-import { ReactElement, useState } from 'react'
+import { ReactElement } from 'react'
 import { SelectWord } from './select'
 
 interface PersonbuttonProps {}
 
 export const Startbutton: (props: PersonbuttonProps) => ReactElement = (props: PersonbuttonProps): ReactElement => {
-  const [pieces, setPieces] = useState<boolean>(false)
-  const [select, setSelect] = useState<boolean>(false)
-
   return (
     <Center h={100}>
       <SelectWord></SelectWord>
